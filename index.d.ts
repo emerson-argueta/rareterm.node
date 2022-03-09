bef9d8da73070f11eddbe43dd2463ad230445a63
emerson-argueta <emerson@emersonargueta.com>
2022-03-09 08:03:55 -0800

    updated types

--- index.d.ts
+++ index.d.ts
@@ -13,7 +13,32 @@ interface TradeCondition {
     with: { type: string, value: number },
 
 }
-type SellOrder = {
+type SignedTrade = {
+    type: string,
+    make: {
+        assetType: {
+            assetClass: string,
+            tokenId: string,
+            contract: string
+        },
+        value: string
+    },
+    take: {
+        assetType: {
+            assetClass: string
+        },
+        value: string
+    },
+    data: {
+        dataType: string,
+        payouts: [],
+        originFees: []
+    },
+    salt: string,
+    maker: string,
+    signature: string
+}
+type TradeReceipt = {
     type: string,
     maker: string,
     make: {
@@ -22,14 +47,9 @@ type SellOrder = {
             contract: string,
             tokenId: string,
             uri: string,
-            creators: [
-                {
-                    account: string,
-                    value: number
-                }
-            ],
-            royalties: Array<any>,
-            signatures: Array<string>
+            creators: [],
+            royalties: [],
+            signatures: []
         },
         value: string
     },
@@ -41,48 +61,62 @@ type SellOrder = {
     },
     fill: string,
     makeStock: string,
-    cancelled: boolean,
+    cancelled: false,
     salt: string,
-    data: {
-        dataType: string,
-        payouts: Array<any>,
-        originFees: Array<any>
-    },
     signature: string,
     createdAt: string,
     lastUpdateAt: string,
-    pending: Array<any>,
-    hash: string,
-    makeBalance: "0",
-    makePriceUsd: number
-}
-type Token = {
-    id: string,
-    contract: string,
-    tokenId: string,
-    creators: [
-        {
-            account: string,
-            value: number
-        }
-    ],
-    supply: string,
-    lazySupply: string,
-    owners: Array<string>,
-    royalties: [],
-    date: string,
     pending: [],
-    deleted: false
+    hash: string,
+    makeBalance: string,
+    makePriceUsd: number,
+    data: {
+        dataType: string,
+        payouts: [],
+        originFees: []
+    }
+}
+type SignedToken = {
+    type: string,
+    tokenId: string,
+    uri: string,
+    tokenURI: string,
+    contract: string,
+    creators: [],
+    royalties: [],
+    signatures: []
+
+}
+type TokenReceipt = {
+    id: string
+    contract: string
+    tokenId: string
+    creators: [],
+    supply: string
+    lazySupply: string
+    owners: [],
+    royalties: [],
+    date: string
+    pending: [],
+    deleted: boolean
+}
+declare class FS {
+    add(item: ArrayBuffer | Blob | File | string): Promise<string>
 }
 declare class Trade {
-    create(tradeCondition: TradeCondition): Promise<SellOrder>
+    create(tradeCondition: TradeCondition): Promise<SignedTrade>
+    send(singedTrad: SignedTrade): Promise<TradeReceipt>
 }
-declare class Rarepress {
+declare class Token {
+    create(token: { type: 'ERC721' | 'ERC1155', metadata: NftMetadata, contract?: string, creators?: NftCreators, royalties?: NftRoyalties, supply?: number }): Promise<SignedToken>
+    send(signedToken: SignedToken): Promise<TokenReceipt>
+}
+declare class Rareterm {
+    fs: FS
+    token: Token
     trade: Trade
     account: string
     init(config: InitConfig)
-    add(item: ArrayBuffer | Blob | File | string): Promise<string>
-    create(token: { type: 'ERC721' | 'ERC1155', metadata: NftMetadata, creators?: NftCreators, royalties?: NftRoyalties, supply?: number }): Promise<Token>
 }
 
-export = Rarepress
+export = Rareterm
